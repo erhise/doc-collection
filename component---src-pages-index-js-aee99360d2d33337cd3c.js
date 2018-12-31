a8989614fdf5beadb5468f8ac0eeb05b6e36b989
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 01:05:12 +0000

    Deploy erhise/doc-collection to github.com/erhise/doc-collection.git:gh-pages

---
+++ component---src-pages-index-js-aee99360d2d33337cd3c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{148:function(n,t,e){"use strict";e.r(t),e.d(t,"query",function(){return r});e(27);var c=e(8),o=(e(0),e(154),e(159)),s=e(158),a={name:"8kn4zf",styles:"margin-top:0;"},i={name:"j7qwjs",styles:"display:flex;flex-direction:column;"},r="2328856904";t.default=function(n){n.data;var t=n.location;return Object(c.b)(o.a,{location:t},Object(c.b)(s.a,null,Object(c.b)("h1",{css:a},"Under construction"),Object(c.b)("div",{css:i})))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-aee99360d2d33337cd3c.js.map

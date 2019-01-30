1022edaebb513432ca2d04550afbf6a1ebd44a19
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-30 15:58:29 +0000

    Deploy erhise/doc-collection to github.com/erhise/doc-collection.git:gh-pages

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0474a931ba8ccb553f78.js"
+    "url": "webpack-runtime-92c3b2bf5e5b9ee84f7d.js"
   },
   {
     "url": "app-6cd8742133a643a5866b.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e277258cfb1d6a5523a8e5637e49955f"
+    "revision": "f75eb2785053e5fbf4dc50933ab40322"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"

ebb9682d8a1ad5ed550b2ffbcbbe3349a110036a
erhise <erikhidrup@gmail.com>
2019-01-30 17:09:46 +0100

    fix: add browser.js

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+exports.onServiceWorkerUpdateFound = () => {
+  const answer = window.prompt(
+    `This application has been updated. ` +
+    `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.reload()
+  }
+}
